3de8a4e161fc697e6dd74826f7a3ae699d5d8584
Blyxyas <blyxyas@gmail.com>
2021-07-28 22:41:47 +0200

    -

--- js/json-editor/r.js
+++ js/json-editor/r.js
@@ -4,13 +4,14 @@ fs = require("fs");
 
 //import the local file
 PATH = "./data.json";
+editTo = "./datax.json"
 data = require(PATH);
 
 // Read the data
-console.log(JSON.stringify(data.d));
-data.d += "hi"
+message = JSON.stringify(data.d);
+message += "si"
 
 // Write to file (in 'data')
-  fs.writeFile('./datax.json ', JSON.stringify(data.d), function (err) {
+  fs.writeFile(editTo, message, function (err) {
     if (err) throw err;
   });
