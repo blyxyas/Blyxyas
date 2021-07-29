4b8e54a1bcc4a96ff44f587a5baf104f151792b1
Blyxyas <blyxyas@gmail.com>
2021-07-29 18:11:22 +0200

    -

--- js/gammagame/app.js
+++ js/gammagame/app.js
@@ -1,3 +1,11 @@
+//************************************************************************************************************************************************
+/*
+^
+^ Copyright 2021 Blyxyas, github.com/Blyxyas. If you want to use this code, you must leave this (current) line intact and legible.
+^ I don't think you're gonna use this code, but I'll leave it in here anyway.
+^
+ */
+// ***********************************************************************************************************************************************
 
 // Recommended: To visualize better this code use the "Collorful Comments" extension in VS Code
 // Import readline and fs modules.
@@ -9,6 +17,8 @@ rl = require("readline").createInterface({
 
 // Import the save file, if it exists, else, create a file named "savestate" and start the game.
 savestate = require("./savestate.json");
+// And import another data module to handle the game's auxiliar variables, commands, etc....
+data = require("./data.json");
 
 // **********************
 // TODO: SAVE FUNCTION ↓↓
@@ -25,20 +35,51 @@ function save() {
     fs.writeFileSync("./savestate.json", JSON.stringify(savestate));
   }
 }
+
 // *******************************************
 // ^ MAIN GAME
 // *******************************************
 
-// ~ Check the argument
+// **********************
+// ~ Commands:
+// **********************
+// ~   help - Shows the list of commands.
+// ~   save - Saves the game state.
+// ~   exit / quit - quits the game (and saves the game state.)
+// ~   inv - Shows the inventory.
+// ~   stats - Shows the player's stats.
+// ~   cat - Shows this cool cat ascii art, you know, for fun.
+
+// TODO: more commands, maybe?
+// ? if you have more commands, create a pull request or something.
+// **********************
 
 rl.on("line", (line) => {
   if (commands.indexOf(line) > -1) {
     if (line === "save") {
       save();
     }
+    if (line === "inv") {
+      console.log(data.inventory);
+    }
+    if (line === "exit" || line === "quit") {
+      console.log(
+        "Goodbye! (All your progress were saved from 'data.json' to 'savestate.json')"
+      );
+      process.exit();
+    }
+    if (line === "cat") {
+      const str = `
+      _._     _,-'""\`-._
+      (,-.\`._,'(       |\`-/|
+          \`-.-' \ )-\`( , o o)
+                \`-    \`_\`"'-`;
+                console.log(str)
+    }
+
   } else {
     console.log(
-      "Invalid command. All your progress were saved to 'savestate.json' don't worry"
+      "Invalid command. All your progress were saved to 'savestate.json', don't worry"
     );
     save();
     process.exit();
