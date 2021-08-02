db4effc20149bdc7acb6d767a5923dd1b466ca39
Blyxyas <blyxyas@gmail.com>
2021-08-02 19:13:26 +0200

    -

--- js/gammagame/game.js
+++
@@ -1,8 +0,0 @@
-// * Import libs and things ↓:
-fs = require("fs");
-rl = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-data = require("./data.json");
-require("./commands.js");
