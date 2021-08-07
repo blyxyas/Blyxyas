d830f37ab717a1ae7da37f15deb9b299b99307d9
Blyxyas <blyxyas@gmail.com>
2021-08-07 20:12:01 +0200

    /

--- quick-search.py
+++ quick-search.py
@@ -5,12 +5,15 @@ listing = [
 
 """
 * There's a Tom Scott's video for this... (https://www.youtube.com/watch?v=RGuJga2Gl_k)
+
 """
 for number in listing:
     if number == target:
         print(
             f"Target Targeted!\nIndex: {listing.index(target)}\nTarget: {target}\nFound between others {len(listing) - 1} elements"
-            )
-        
+        )
+
     else:
         continue
+    
+    # * This ⬆ isn't the algorithm.
