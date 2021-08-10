cbf3c99d776cec0c51fdeec2f64368a8c2ece1de
Blyxyas <blyxyas@gmail.com>
2021-08-10 14:18:12 +0200

    Update fizzbuzz.py

--- simplest/fizzbuzz.py
+++ simplest/fizzbuzz.py
@@ -1,8 +1,5 @@
 
 # * This is the optimized approach (Optimized just by like 0.008 s) ↓
-import time
-start_time = time.time()
-
 desired = 100 # * desired number of iterations, you can change it
 
 for x in range(desired + 1):
@@ -10,12 +7,10 @@ for x in range(desired + 1):
     if x % 3 == 0:
         output += "Fizz"
         if x % 5 == 0:
-            output += "Buzz"
+            output += "Buzzj"
     elif x % 5 == 0:
             output = "Buzz"
     if output == "":
         print(x)
     else:
         print(output)
-        
-print("--- %s seconds ---" % (time.time() - start_time))
