7459ace57c6148b1d9e2814e5b6991a0d1bcf018
Blyxyas <blyxyas@gmail.com>
2021-08-18 14:08:52 +0200

    -

--- Blyx-Lang/main.py
+++ Blyx-Lang/main.py
@@ -4,28 +4,38 @@ README 2:
 
 import sys
 
-with open("template.blyx") as f:
+with open("output.txt", "w") as new:
+    new.write("")
+
+with open("Template.blyx") as f:
     file = f.read()
 
 list_ = file.split(" ") 
 
+def writing(argument):
+    with open("output.txt", "a") as fl:
+        fl.write(argument)
+
 n = 0
 while n < len(list_):
-    
+    print("Loop Entered")
     arg = list_[n]
     
     err = f"Error in argument {n} ({arg}): \n"
     function_name = ""
 
-    if arg == "func":
-        if list_[n + 1].split()[-1] != ".":
-            print(err)
-            print("Function must end with a \".\"")
-            sys.exit()
-        else:
-         print(f"Line {n}")
+    print("function detect ed")
+    if arg.startswith("\n--"):
+        # * That's a function
+        function_name = arg[3:-3]
+        writing(f"def {function_name}:")
         
-         
-    
+        in_function = True
+        
+    while in_function == True:
+        arg = "\t".join(arg)
+        break
+    else:
+        sys.exit()
 
     n += 1
