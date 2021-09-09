3b75f2f3807caa04391d4ddb04dc519a338146c8
Blyxyas <blyxyas@gmail.com>
2021-09-09 17:52:28 +0200

    minesweeper for discord

---
+++ discord-minesweeper-builder/main.py
@@ -0,0 +1,27 @@
+with open("input.txt", "r") as f:
+    stdin = f.read()
+    f.close()
+def writing(message):
+    with open("output.txt", "w") as f:
+        f.write(message)
+        f.close()
+
+switch = {
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
+    "b": "bomb"
+}
+
+appending = []
+for arg in stdin:
+    current = switch.get(str(arg), "ERROR, Invalid Character")
+    appending.append(f"||:{current}:||")
+
+writing("".join(appending))
